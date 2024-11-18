fce9f166a1c7a13bc1c9779b258b38e8f5772ed2
trendzpro <arabiantrendzpro@gmail.com>
2024-11-18 14:53:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// code for fin
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeInElements = document.querySelectorAll(".fade-in");
+    fadeInElements.forEach((el) => {
+      el.style.opacity = "1";
+    });
+  });
+
+// code for buy
+function buyProduct(productName, productPrice) {
+    alert(`You have purchased ${productName} for $${productPrice}.`);
+}
+// code for thank u noti...
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const successMessage = document.createElement('div');
+    successMessage.textContent = 'Thank you for your message! We will get back to you soon.';
+    successMessage.className = 'success-message fade-in';
+    document.querySelector('.contact-form').appendChild(successMessage);
+
+    this.reset();
+});
